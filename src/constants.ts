f89507abaceee49f2a085bdc0c6166aa60448245
CodingNeko <codingneko@gmail.com>
2023-03-31 10:03:34 +0200

    Delete constants.ts
    WHY WAS THIS COMMITED IT'S IN THE GIT IGNORE REEEEEEEEEEEEEEEEEEE

--- src/constants.ts
+++
@@ -1,7 +0,0 @@
-export const constants = {
-    url: 'codingneko.com',
-    misskeyApiUrl: 'https://misskey.codingneko.com/api',
-    githubApiUrl: 'https://api.github.com',
-    githubAuthToken: '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',
-    misskeyAuthToken: 'nw3QcepNChM6oxM3m9R9alF5RE8YsxAQ',
-};
